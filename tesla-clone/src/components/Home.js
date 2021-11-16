f119e1355dd353d6e4cb182d6d3f051ac5c0b2ef
Abhimanyu-dev <abhimanyusolanki4841@gmail.com>
2021-11-17 02:49:20 +0530

    Done

--- tesla-clone/src/components/Home.js
+++ tesla-clone/src/components/Home.js
@@ -5,7 +5,54 @@ import Section from './Section'
 function Home() {
     return (
         <Container>
-            <Section />
+            <Section 
+                title="Model S"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-s.jpg"
+                LftBtnTxt="Custom Order"
+                RtBtnTxt="Existing Inventory"
+            />
+            <Section
+                title="Model Y"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                LftBtnTxt="Custom Order"
+                RtBtnTxt="Existing Inventory"
+            />
+            <Section
+                title="Model 3"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-3.jpg"
+                LftBtnTxt="Custom Order"
+                RtBtnTxt="Existing Inventory"
+            />
+            <Section
+                title="Model X"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                LftBtnTxt="Custom Order"
+                RtBtnTxt="Existing Inventory"/>
+            <Section
+                title="Solar Panels"
+                description="Lowest Cost Solar Panels in America"
+                backgroundImg="solar-panel.jpg"
+                LftBtnTxt="Order Now"
+                RtBtnTxt="Learn More"
+            />
+            <Section
+                title="Solar Roof"
+                description="Produce Clean Energy From Your Roof
+                "
+                backgroundImg="solar-roof.jpg"
+                LftBtnTxt="Order Now"
+                RtBtnTxt="Learn More"
+            />
+            <Section
+                title="Accessories"
+                description=""
+                backgroundImg="accessories.jpg"
+                LftBtnTxt="Shop Now"
+            />
         </Container>
     )
 }
@@ -15,6 +62,4 @@ export default Home
 
 const Container = styled.div`
         height: 100vh;
-        font-size: 2.5vh;
-        color: (255,0,0);
 `
--- tesla-clone/src/components/Section.js
+++ tesla-clone/src/components/Section.js
@@ -1,10 +1,91 @@
 import React from 'react'
+import styled from 'styled-components'
 
-function Section() {
+function Section({title, description, LftBtnTxt, RtBtnTxt, backgroundImg}) {
     return (
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur facere, commodi aliquam ab nostrum nesciunt, consectetur quasi eum necessitatibus eos, cum explicabo laboriosam labore nobis illo molestiae repudiandae eius magni qui? Earum consequuntur illum enim dolores numquam quam. Rerum deleniti eveniet corrupti modi veritatis, suscipit quas accusamus vero fuga reiciendis impedit similique minima? Maiores veritatis nisi dolorum nobis eligendi vero non eveniet ratione, modi voluptates, natus suscipit ex quidem unde delectus itaque distinctio? Facere, expedita rerum alias ratione iusto, numquam temporibus, aliquam earum facilis accusantium eum dolor dolore sed ipsum repudiandae. Voluptatem ipsam itaque iste, similique dolores quae eum pariatur, totam ex aut sunt cum voluptate. Totam eum, quo ipsa possimus similique officiis beatae deleniti iste debitis, cumque eligendi dolorem omnis voluptatibus a molestiae voluptate asperiores molestias laboriosam nostrum facilis. Laboriosam dicta facere quaerat quidem aspernatur qui mollitia deserunt praesentium, aliquid officiis ratione alias iste numquam ipsum inventore, corrupti illo, asperiores excepturi temporibus ipsam ad reiciendis corporis animi? Quas hic quam laboriosam magnam ducimus aliquam illum ipsum mollitia, neque quae atque quidem id recusandae voluptatibus nesciunt optio qui blanditiis aut labore. Necessitatibus amet veniam illo ut nesciunt facilis dolore fugit non dolor voluptates, velit nisi natus cum magnam quibusdam voluptas.
-    </div>
+        <Wrap bg={backgroundImg}>
+           <ItemText>
+               <h1>
+                   {title}
+               </h1>
+               <p>
+                   {description}
+               </p>
+           </ItemText>
+           <Buttons>
+           <ButtonGroup>
+                <LeftButton>
+                    {LftBtnTxt}
+                </LeftButton>
+                {RtBtnTxt && 
+                <RightButton>
+                    {RtBtnTxt}
+                </RightButton>}
+                
+           </ButtonGroup>
+           <DownArrow src="/images/down-arrow.svg">
+           </DownArrow>
+           </Buttons>
+    </Wrap>
     )
 }
 
 export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: ${props => `url("/images/${props.bg}")`};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items:center;
+`
+ const ItemText= styled.div
+ `
+    padding-top: 15vh;
+    text-align: center;
+ `
+ const ButtonGroup= styled.div`
+    display: flex;
+    margin-bottom: 30px;
+    @media (max-width:768px){
+        flex-direction:column;
+    }
+
+    
+ `
+ const LeftButton= styled.div`
+    background-color: rgba(23,26,32,0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px; 
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size:12px;
+    cursor: pointer;
+    margin: 8px;
+ `
+
+ const RightButton= styled(LeftButton)`
+    background-color: white;
+    opacity: .85;
+    color: black;
+
+ `
+
+ const DownArrow = styled.img`
+    height: 40px;
+    animation: animateDown infinite 1.5s;
+    overflow-x: hidden;
+ `
+
+ const Buttons= styled.div``
+
