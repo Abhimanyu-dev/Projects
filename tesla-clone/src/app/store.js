1c4ff1388c897256cf09afe5ab4c7056c759f549
Abhimanyu-dev <abhimanyusolanki4841@gmail.com>
2021-11-18 00:18:49 +0530

    removed counter

--- tesla-clone/src/app/store.js
+++ tesla-clone/src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import CarReducer from "../features/Car/CarSlice"
 
 export const store = configureStore({
